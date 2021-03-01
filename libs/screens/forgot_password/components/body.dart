343f8420865044cf26bd396d5f3ef662b98bf623
erdemsword <erik001.ronaldinho@gmail.com>
2021-03-01 22:23:27 +0300

    Create body.dart

---
+++ libs/screens/forgot_password/components/body.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:e_commerce_ui/components/custom_suffix_icon.dart';
+import 'package:e_commerce_ui/components/form_error.dart';
+import 'package:e_commerce_ui/components/no_account_text.dart';
+import 'package:e_commerce_ui/size_config.dart';
+
+import '../../../constants.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: Column(
+            children: [
+              SizedBox(height: SizeConfig.screenHeight * 0.04),
+              Text(
+                "Şifremi unuttum",
+                style: TextStyle(
+                  fontSize: getProportionateScreenWidth(28),
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Lütfen e-posta adresinizi girin, hesabınıza giriş yapmanız için \nsize bir bağlantı göndereceğiz",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.1),
+              ForgotPassForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ForgotPassForm extends StatefulWidget {
+  @override
+  _ForgotPassFormState createState() => _ForgotPassFormState();
+}
+
+class _ForgotPassFormState extends State<ForgotPassForm> {
+  final _formKey = GlobalKey<FormState>();
+  List<String> errors = [];
+  String email;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            onSaved: (newValue) => email = newValue,
+            onChanged: (value) {
+              if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+                setState(() {
+                  errors.remove(kEmailNullError);
+                });
+              } else if (emailValidatorRegExp.hasMatch(value) &&
+                  errors.contains(kInvalidEmailError)) {
+                setState(() {
+                  errors.remove(kInvalidEmailError);
+                });
+              }
+              return null;
+            },
+            validator: (value) {
+              if (value.isEmpty && !errors.contains(kEmailNullError)) {
+                setState(() {
+                  errors.add(kEmailNullError);
+                });
+              } else if (!emailValidatorRegExp.hasMatch(value) &&
+                  !errors.contains(kInvalidEmailError)) {
+                setState(() {
+                  errors.add(kInvalidEmailError);
+                });
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              labelText: "Eposta",
+              hintText: "Epostanızı buraya yazın",
+              // If  you are using latest version of flutter then lable text and hint text shown like this
+              // if you r using flutter less then 1.20.* then maybe this is not working properly
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+            ),
+          ),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          FormError(errors: errors),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          RaisedButton(
+            textColor: Colors.white,
+            padding: EdgeInsets.all(0.0),
+            shape: StadiumBorder(),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFF00b09b),
+                    Color(0xFF96c93d),
+                  ],
+                ),
+              ),
+              child: Text(
+                'Devam et',
+                style: TextStyle(fontSize: 15.0),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 70.0, vertical: 15.0),
+            ),
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                // Do what you want to do
+              }
+            },
+          ),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          NoAccountText(),
+        ],
+      ),
+    );
+  }
+}
